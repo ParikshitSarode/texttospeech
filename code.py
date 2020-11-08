6ec7700bd5424f94041f5db6e05908f073e06cba
Parikshit Sarode <sarode.parikshit@gmail.com>
2020-11-08 11:09:52 +0530

    commit
    commit

---
+++ code.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+
+import os
+
+f = open('textfile.txt')
+
+x = f.read()
+
+lang = 'en'
+
+audio=gTTS(text = x, lang = lang, slow= False)
+
+audio.save("speech.wav")
+
+os.system("speech.wav") 
